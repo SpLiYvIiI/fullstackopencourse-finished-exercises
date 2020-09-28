a9d955ba2bd211c47025fcc738b1b12971081166
SpLiYvIiI <atukadagio123@gmail.com>
2020-09-28 18:59:21 +0400

    neww

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Button = ({func,text})=> <><button onClick={func}>{text}</button></>
+const Statsitics = ({good,neutral,bad})=>{
+  let all = good+neutral+bad;
+  let average = all === 0? 0 : (good+ (-bad))/all;
+  let percentage = all === 0 ? 0 : (good/all)*100;
+  if(all === 0 ) return(<p>No feedback given</p>)
+  else{
+  return(
+  <div>
+  <p>Good : {good}</p>
+  <p>Neutral : {neutral}</p>
+  <p>Bad : {bad}</p>
+  <p>All : {all}</p>
+  <p>Average : {average}</p>
+  <p>Percentage : {percentage}%</p>
+  </div>
+  )
+  }
+}
+const App = ()=>{
+  const [good,setGood] = useState(0);
+  const [neutral,setNeutral] = useState(0);
+  const [bad,setBad] = useState(0);
+  return (
+      <div> 
+        <h1>Give Feedback</h1>
+        <Button func={()=>{setGood(good+1)}} text="good"/>
+        <Button func={()=>{setNeutral(neutral+1)}} text="neutral"/>
+        <Button func={()=>{setBad(bad+1)}} text="bad"/>
+        <Button func={()=>{setBad(0);setGood(0);setNeutral(0)}} text="reset"/>
+        <h1>Statistics</h1>
+        <Statsitics  good={good} neutral={neutral} bad={bad} />
+      </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
