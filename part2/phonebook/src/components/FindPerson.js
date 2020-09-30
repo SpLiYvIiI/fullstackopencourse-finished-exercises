bab87cddeb9e458104dd4500f4ec139cd70ed237
SpLiYvIiI <atukadagio123@gmail.com>
2020-09-30 12:52:06 +0400

    Update FindPerson.js

--- part2/phonebook/src/components/FindPerson.js
+++ part2/phonebook/src/components/FindPerson.js
@@ -3,10 +3,13 @@ import React from 'react'
 
 export default ({Filter,setPerson,Person}) => {
     return(
+        <div>
+        search for people
         <input 
         value={Person}
         onChange={(event)=>{setPerson(event.target.value); Filter(event.target.value)}}
         >   
         </input>
+        </div>
     )
 }
