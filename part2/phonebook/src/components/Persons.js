06a991ce32f5b05b36e5136c1a9dd401719d306c
SpLiYvIiI <atukadagio123@gmail.com>
2020-10-03 17:15:42 +0400

    key

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default ({persons,deleteNumber}) => persons.map(person => 
-<p className='person'>{person.name} {person.number} <button onClick={()=>{deleteNumber(person.id)}}>delete</button> </p>
+export default ({persons,deleteNumber}) => persons.map((person,i) => 
+<p key={i} className='person'>{person.name} {person.number} <button onClick={()=>{deleteNumber(person.id)}}>delete</button> </p>
 )
