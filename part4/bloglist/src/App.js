f2e5f82f095acbeb055ff7c3a1cf6a74d93361c9
SpLiYvIiI <atukadagio123@gmail.com>
2020-10-12 15:55:58 +0400

    update

--- part4/bloglist/src/App.js
+++ part4/bloglist/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import BlogService from './services/Blogservice'
+import Blogs from './components/Blogs'
 
 
 export default ()=>{
-  const [Blogs,setBlogs] = useState([])
+  const [blogs,setBlogs] = useState([])
   useEffect(()=>{
     BlogService.getAll().then(InitialData=>{
       setBlogs(InitialData)
@@ -12,6 +13,7 @@ export default ()=>{
   return(
     <div>
     <h1>Blog List</h1>
+    <Blogs Blogs={blogs}/>
     </div>
     )
 }
